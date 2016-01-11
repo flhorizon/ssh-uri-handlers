8d6357bb725fc54bb71338aeecd33c3adcb79dfc
Florian Fouratier <ffourati@student.42.fr>
2016-01-11 11:43:35 +0100

    Using sshpass to forward an explicitly provided password.

--- linux/lxde/README.md
+++ linux/lxde/README.md
@@ -1,2 +1,8 @@
+## Additional requirements
+
+_sshpass_   
+   
+When the password is explicitly provided in the URI, `sshpass` is executed with the provided password and the ssh command.
+
 ## Action
 Executes `lxterminal` with a command running `ssh`, passing the URI's parameters; handing back a regular shell afterwards.
--- linux/lxde/lxde-handle-ssh-uri.rb
+++ linux/lxde/lxde-handle-ssh-uri.rb
@@ -1,12 +1,19 @@
 #!/usr/bin/ruby
 
+
 HandOver_tok = '--DO'
 unless ARGV[0] == HandOver_tok
+
     # Execute LXDEterminal and make it run the rest of this script,
     # forwarding the URI argument.
     shell = ENV['SHELL'] || '/bin/sh'
-    command = %Q|lxterminal --working-directory=#{ENV['HOME']} --command="/bin/sh -c '#{$0} #{HandOver_tok} #{ARGV.join ' '}; #{shell}'"|
-    exec command
+    work_dir = ENV['HOME'] || '/var/tmp'
+
+    # $0 is either run with its absolute path or searched in PATH.
+    command = %q|lxterminal --working-directory=%s --command="/bin/sh -c '%s %s %s; %s'"|
+    interp = [] << work_dir << $0 << HandOver_tok << ARGV.join(' ') << shell
+
+    exec command % interp
 end
 
 require 'addressable/uri'
@@ -15,11 +22,13 @@ uri = Addressable::URI.parse ARGV[1]
 exit unless uri and uri.scheme == 'ssh'
 
 port_tok = uri.port ? "-p #{uri.port.to_s}" : ''
-cred_tok = uri.user ? uri.user : ''
-if cred_tok.length > 0
-    passwd_tok = uri.password ? ":#{uri.password}" : ''
-    cred_tok << passwd_tok << '@'
+user_tok = uri.user ? "#{uri.user}@" : ''
+if uri.password
+    ENV['SSHPASS'] = uri.password
+    passwd_tok = 'sshpass -e'
+else
+    passwd_tok = ''
 end
 
-command = "ssh #{port_tok} #{cred_tok}#{uri.host}"
+command = %Q|#{passwd_tok} ssh #{port_tok} #{user_tok}#{uri.host}|
 exec command
